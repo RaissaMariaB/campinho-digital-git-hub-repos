148a9bd75737199e3eb4ef9c94f68ab55ebb5534
Raissa Barata Pereira <raissamaria.sb@gmail.com>
2023-11-22 00:38:20 -0300

    remove dados do projeto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
+//Instalar o axios com a turma 
 import './App.css';
 
 
@@ -10,40 +11,23 @@ function App() {
 
   //primeira etapa apenas chamada nativa, sem o Axios
   useEffect(() => {
-    fetch(`${baseUrl}users/raissamariab/repos`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setRepos(data);
-        return data;
-    })
-    .catch(e => {
-      return  console.log(e);
-    })
+  
   }, [])
 
     //Segunda etapa com o Axios
     useEffect(() => {
-      axios(`${baseUrl}users/raissamariab/repos`)
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-          setRepos(data);
-          return data;
-      })
-      .catch(e => {
-        return  console.log(e);
-      })
+     
     }, [])
 
   return (
         <ul>
           {repos.map((cadaRepo) => {
-            return (<li>
-              {cadaRepo?.name}
-            </li>)
-          })
-        }
+            return (
+              <li>
+                {cadaRepo?.name}
+              </li>)
+            })
+          }
         </ul>
   );
 }
